a87b9110798282c656f6e51cd60543d26ebe2f23
mirsazzathossain <mirsazzathossain@gmail.com>
2021-07-06 12:34:41 +0600

    Setted up database and user models!

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+def signin(request):
+    return HttpResponse("This is log in page!")
+
+def signup(request):
+    return HttpResponse("This is register page!")
+
+def verify(request):
+    return HttpResponse("This is verify-email page!")
